4cc91995a8069b1ae52236584bc26dffc99310af
matt imdad <mimdad@bsd20.org>
2020-01-10 10:43:25 -0600

    added some images and some code from schoology

---
+++ City.swift
@@ -0,0 +1,13 @@
+//
+//  City.swift
+//  Favorite Cities
+//
+//  Created by matt imdad on 1/10/20.
+//  Copyright © 2020 matt imdad. All rights reserved.
+//
+
+import UIKit
+
+class City: Codeable {
+
+}
