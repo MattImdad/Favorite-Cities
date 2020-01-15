6e2862aa01c2aef4632a235b3a80721d07770cb1
matt imdad <mimdad@bsd20.org>
2020-01-15 10:50:19 -0600

    enabled data persistence

--- Favorite Cities/MasterViewController.swift
+++ Favorite Cities/MasterViewController.swift
@@ -12,6 +12,7 @@ class MasterViewController: UITableViewController {
     
     var detailViewController: DetailViewController? = nil
     var cities = [City]()
+    let defaults = UserDefaults.standard
     
     
     override func viewDidLoad() {
@@ -25,12 +26,19 @@ class MasterViewController: UITableViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+        
+        if let savedData = defaults.object(forKey: "data") as? Data {
+             if let decoded = try? JSONDecoder().decode([City].self, from: savedData) {
+                   cities = decoded
+             }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
         tableView.reloadData()
+        saveData()
     }
     
     @objc
@@ -62,62 +70,71 @@ class MasterViewController: UITableViewController {
                                 image: image.pngData()!)
                 self.cities.append(city)
                 self.tableView.reloadData()
+                self.saveData()
             }
         }
         alert.addAction(insertAction)
         present(alert, animated: true, completion: nil)
     }
-        
-        // MARK: - Segues
-        
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if segue.identifier == "showDetail" {
-                if let indexPath = tableView.indexPathForSelectedRow {
-                    let object = cities[indexPath.row]
-                    let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                    controller.detailItem = object
-                    controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-                    controller.navigationItem.leftItemsSupplementBackButton = true
-                    detailViewController = controller
-                }
-            }
-        }
-        
-        // MARK: - Table View
-        
-        override func numberOfSections(in tableView: UITableView) -> Int {
-            return 1
-        }
-        
-        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return cities.count
-        }
-        
-        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-            let object = cities[indexPath.row]
-            cell.textLabel!.text = object.name
-            return cell
-        }
-        
-        override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-            // Return false if you do not want the specified item to be editable.
-            return true
-        }
-        
-        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            if editingStyle == .delete {
-                cities.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            } else if editingStyle == .insert {
-                // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-            }
-        }
-        
-        override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-            let objectToMove = cities.remove(at: sourceIndexPath.row)
-            cities.insert(objectToMove, at: destinationIndexPath.row)
+    
+    func saveData() {
+        if let encoded = try? JSONEncoder().encode(cities) {
+            defaults.set(encoded, forKey: "data")
         }
     }
+    
+    // MARK: - Segues
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let object = cities[indexPath.row]
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+                detailViewController = controller
+            }
+        }
+    }
+    
+    // MARK: - Table View
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let object = cities[indexPath.row]
+        cell.textLabel!.text = object.name
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            cities.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let objectToMove = cities.remove(at: sourceIndexPath.row)
+        cities.insert(objectToMove, at: destinationIndexPath.row)
+}
 
 
+
+
+}
